fe48ae833847cf62069ebfa612f6f7ebffc652b5
BuildTools <unconfigured@null.spigotmc.org>
2022-03-25 00:45:05 +0100

    fix

--- src/main/java/pl/starozytny/events/PlayerKill.java
+++ src/main/java/pl/starozytny/events/PlayerKill.java
@@ -4,6 +4,7 @@ import com.alonsoaliaga.alonsoleagues.api.AlonsoLeaguesAPI;
 import com.alonsoaliaga.alonsolevels.api.AlonsoLevelsAPI;
 import com.yapzhenyie.GadgetsMenu.api.GadgetsMenuAPI;
 import com.yapzhenyie.GadgetsMenu.player.PlayerManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -37,11 +38,6 @@ public class PlayerKill implements Listener {
 		}
 
 		if (PlayerUtil.hasPerm(killer, "mnoznik.gracz")) {
-			AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.DEFAULT_EXPERIENCE));
-			playerKiller.addMysteryDust(Integer.parseInt(config.DEFAULT_MONEY));
-			AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.DEFAULT_POINTS_ADD));
-			AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.DEFAULT_POINTS_REMOVE));
-
 			messages_victim = config.MESSAGES_DEFAULT_VICTIM.stream().filter(Objects::nonNull).map(rawList -> rawList.
 					replace("{victim}", victim.getName()).
 					replace("{killer}", killer.getName()).
@@ -58,7 +54,19 @@ public class PlayerKill implements Listener {
 					replace("{experience}", config.DEFAULT_EXPERIENCE).
 					replace("{money}", config.DEFAULT_MONEY)).collect(Collectors.toList());
 
-			Common.tell(victim, messages_victim);
+
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLevels")) {
+				AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.DEFAULT_EXPERIENCE));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("GadgetsMenu")) {
+				playerKiller.addMysteryDust(Integer.parseInt(config.DEFAULT_MONEY));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLeagues")) {
+				AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.DEFAULT_POINTS_ADD));
+				AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.DEFAULT_POINTS_REMOVE));
+				Common.tell(victim, messages_victim);
+			}
+
 			Common.tell(killer, messages_killer);
 
 			if (config.DEBUG) {
@@ -67,11 +75,6 @@ public class PlayerKill implements Listener {
 			}
 
 		} else if (PlayerUtil.hasPerm(killer, "mnoznik.vip")) {
-			AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.VIP_EXPERIENCE));
-			playerKiller.addMysteryDust(Integer.parseInt(config.VIP_MONEY));
-			AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.VIP_POINTS_ADD));
-			AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.VIP_POINTS_REMOVE));
-
 			messages_victim = config.MESSAGES_VIP_VICTIM.stream().filter(Objects::nonNull).map(rawList -> rawList.
 					replace("{victim}", victim.getName()).
 					replace("{killer}", killer.getName()).
@@ -89,7 +92,18 @@ public class PlayerKill implements Listener {
 					replace("{money}", config.VIP_MONEY)).collect(Collectors.toList());
 
 
-			Common.tell(victim, messages_victim);
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLevels")) {
+				AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.VIP_EXPERIENCE));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("GadgetsMenu")) {
+				playerKiller.addMysteryDust(Integer.parseInt(config.VIP_MONEY));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLeagues")) {
+				AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.VIP_POINTS_ADD));
+				AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.VIP_POINTS_REMOVE));
+				Common.tell(victim, messages_victim);
+			}
+
 			Common.tell(killer, messages_killer);
 
 			if (config.DEBUG) {
@@ -98,11 +112,6 @@ public class PlayerKill implements Listener {
 			}
 
 		} else if (PlayerUtil.hasPerm(killer, "mnoznik.vip+")) {
-			AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.SVIP_EXPERIENCE));
-			playerKiller.addMysteryDust(Integer.parseInt(config.SVIP_MONEY));
-			AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.SVIP_POINTS_ADD));
-			AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.SVIP_POINTS_REMOVE));
-
 			messages_victim = config.MESSAGES_SVIP_VICTIM.stream().filter(Objects::nonNull).map(rawList -> rawList.
 					replace("{victim}", victim.getName()).
 					replace("{killer}", killer.getName()).
@@ -119,7 +128,18 @@ public class PlayerKill implements Listener {
 					replace("{experience}", config.SVIP_EXPERIENCE).
 					replace("{money}", config.SVIP_MONEY)).collect(Collectors.toList());
 
-			Common.tell(victim, messages_victim);
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLevels")) {
+				AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.SVIP_EXPERIENCE));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("GadgetsMenu")) {
+				playerKiller.addMysteryDust(Integer.parseInt(config.SVIP_MONEY));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLeagues")) {
+				AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.SVIP_POINTS_ADD));
+				AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.SVIP_POINTS_REMOVE));
+				Common.tell(victim, messages_victim);
+			}
+
 			Common.tell(killer, messages_killer);
 
 			if (config.DEBUG) {
@@ -128,11 +148,6 @@ public class PlayerKill implements Listener {
 			}
 
 		} else if (PlayerUtil.hasPerm(killer, "mnoznik.mvp")) {
-			AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.MVIP_EXPERIENCE));
-			playerKiller.addMysteryDust(Integer.parseInt(config.MVIP_MONEY));
-			AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.MVIP_POINTS_ADD));
-			AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.MVIP_POINTS_REMOVE));
-
 			messages_victim = config.MESSAGES_MVIP_VICTIM.stream().filter(Objects::nonNull).map(rawList -> rawList.
 					replace("{victim}", victim.getName()).
 					replace("{killer}", killer.getName()).
@@ -149,7 +164,18 @@ public class PlayerKill implements Listener {
 					replace("{experience}", config.MVIP_EXPERIENCE).
 					replace("{money}", config.MVIP_MONEY)).collect(Collectors.toList());
 
-			Common.tell(victim, messages_victim);
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLevels")) {
+				AlonsoLevelsAPI.addExperience(killer.getUniqueId(), Integer.parseInt(config.MVIP_EXPERIENCE));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("GadgetsMenu")) {
+				playerKiller.addMysteryDust(Integer.parseInt(config.MVIP_MONEY));
+			}
+			if (Bukkit.getPluginManager().isPluginEnabled("AlonsoLeagues")) {
+				AlonsoLeaguesAPI.addPoints(killer.getUniqueId(), Integer.parseInt(config.MVIP_POINTS_ADD));
+				AlonsoLeaguesAPI.removePoints(victim.getUniqueId(), Integer.parseInt(config.MVIP_POINTS_REMOVE));
+				Common.tell(victim, messages_victim);
+			}
+
 			Common.tell(killer, messages_killer);
 
 			if (config.DEBUG) {
